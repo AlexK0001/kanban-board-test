f7e48c22e3f82d4716e852fed5481fa33b015057
Alexandr <alexandr.kopyletz@gmail.com>
2025-03-01 22:23:01 +0200

    4

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Provider } from 'react-redux';
-import store from './store/store';
+// import { Provider } from 'react-redux';
+// import store from './store/store';
 // import 'antd/dist/antd.css';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
